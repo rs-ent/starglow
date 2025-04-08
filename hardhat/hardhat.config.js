3a4aa5f15324c77eacb725b8ce3b1e50600f2464
WaydCloud <goldencho3@gmail.com>
2025-04-08 19:08:20 +0900

    alchemy

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,33 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: {
-    version: "0.8.20",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
-  },
-  networks: {
-    mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
-      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
-      chainId: 80001,
+    solidity: {
+        version: "0.8.20",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+        },
     },
-    polygon: {
-      url: "https://polygon-rpc.com",
-      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
-      chainId: 137,
-    }
-  },
-  etherscan: {
-    apiKey: POLYGONSCAN_API_KEY
-  }
+    networks: {
+        amoy: {
+            url: process.env.AMOY_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY],
+        },
+    },
+    etherscan: {
+        apiKey: process.env.POLYGONSCAN_API_KEY,
+    },
 };
