7046c8f7c5cda3ccf0418a51a4fd64280094345b
WaydCloud <goldencho3@gmail.com>
2025-06-10 14:53:30 +0900

    tele

--- components/atoms/TelegramLoginButton.tsx
+++ components/atoms/TelegramLoginButton.tsx
@@ -44,7 +44,7 @@ export default function TelegramLoginButton({
         const script = document.createElement("script");
         script.src = "https://telegram.org/js/telegram-widget.js?22";
         script.async = true;
-        script.setAttribute("data-telegram-login", "starglow_redslippers_bot");
+        script.setAttribute("data-telegram-login", "Waydcloud_bot");
         script.setAttribute("data-size", size);
         script.setAttribute("data-userpic", "false");
         script.setAttribute("data-radius", "6");
