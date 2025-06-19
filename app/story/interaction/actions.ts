9e4e2cae21e529faaf6e50e6109d682dc71b64e8
rs-ent <rs.ent.contact@gmail.com>
2025-06-19 14:33:20 +0900

    verify nft api

--- app/story/interaction/actions.ts
+++ app/story/interaction/actions.ts
@@ -73,3 +73,44 @@ export async function getUserVerifiedSPGs(
         return [];
     }
 }
+
+export async function getWalletAddressVerifiedSPGs(
+    address: string
+): Promise<VerifiedSPG[]> {
+    if (!address) {
+        console.log("[getWalletAddressVerifiedSPGs] No address", address);
+        return [];
+    }
+
+    try {
+        const spgs = await prisma.story_spg.findMany({
+            include: {
+                artist: true,
+                network: true,
+            },
+        });
+
+        const spgsWithTokenOwners = await Promise.all(
+            spgs.map(async (spg) => {
+                const tokenOwners = await getOwners({
+                    spgAddress: spg.address,
+                });
+                const verifiedTokens = tokenOwners
+                    .filter(
+                        (data) =>
+                            data.owner.toLowerCase() === address.toLowerCase()
+                    )
+                    .map((data) => Number(data.tokenId));
+                return {
+                    ...spg,
+                    verifiedTokens,
+                };
+            })
+        );
+
+        return spgsWithTokenOwners;
+    } catch (error) {
+        console.error("Error getting user verified SPGs:", error);
+        return [];
+    }
+}
