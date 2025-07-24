554bdb7f154d0abac741c65da7a5676bf84d0ac8
rs-ent <rs.ent.contact@gmail.com>
2025-07-24 23:37:15 +0900

    Raffle tab change

--- components/raffles/onchain/Raffle.Onchain.Hero.tsx
+++ components/raffles/onchain/Raffle.Onchain.Hero.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { memo, useCallback, useMemo, useState } from "react";
+import { memo, useMemo, useState } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { cn } from "@/lib/utils/tailwind";
